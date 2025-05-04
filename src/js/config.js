e646ef1f9bd97a3417e428262ffb1291d114a92d
Tamara Tylik <tylik.tamara@yandex.ru>
2025-05-04 20:26:04 +0300

    feat:project v.1.0.0

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '013115c1-8591-4d0a-8c9b-e439dd772dee';
+export const MODAL_CLOSE_SEC = 2.5;
